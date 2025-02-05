265692406f70f9f8fe981d7c202e08d273e18dd5
mmanjhu205 <manishmanjhu34@gmail.com>
2025-02-05 07:23:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,14 +3,14 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
+API_ID = int(getenv("API_ID", "28549267"))
+API_HASH = getenv("API_HASH", "b2526517ed963b1951a811b24bc29a4e")
+BOT_TOKEN = getenv("BOT_TOKEN", "7472562415:AAGgwS6eQ_945W7enn3PhYoVfluxD_xvSDw")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8054489757").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Mmanjhu:O9UjosQUi9wWkfAd@cluster0.ukzu1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = getenv("LOG_GROUP", "")
 CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "5"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
 AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f2b7bc2943f618e78cb")
